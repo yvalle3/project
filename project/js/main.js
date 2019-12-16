eb6b63e979ed5b317f548a6a4707d83122adfaba
yvalle3 <47132992+yvalle3@users.noreply.github.com>
2019-12-15 20:40:38 -0600

    Add files via upload

--- project/js/main.js
+++ project/js/main.js
@@ -5,3 +5,19 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  $(document).ready(function () {
+
+    $("button").click(function () {
+    $('#aaa').addClass('contact');
+    });
+    
+    $('#remove').click(function () {
+    $('#aaa').removeClass('contact');
+    })
+    
+    setTimeout(function () {
+    $('#aaa').addClass('contact');
+    }, 2000);
+    
+    })
